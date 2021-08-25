e0aa82007c21db94850e1e27aa3aa920a5e1881e
jfra1397 <jfra@hotmail.de>
2021-08-25 16:20:11 +0200

    included multi-classes

---
+++ results/julian/vgg16_2/create_model.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten
+
+def generate_model():
+    
+    # encoder = tf.keras.applications.MobileNetV2(include_top=False, weights='imagenet', input_shape=(256, 256, 3), classifier_activation=None)
+    encoder = tf.keras.applications.VGG16(include_top=False, weights="imagenet", input_tensor=None, input_shape=(256, 256, 3), pooling=None, classifier_activation= None)
+    encoder.trainable = False
+
+    d1 = UpSampling2D(size=(2, 2))(encoder.layers[-1].output)
+    c1 = Conv2D(8, kernel_size=(3, 3), activation='selu', padding='same')(d1)
+    d2 = UpSampling2D(size=(2, 2))(c1)
+    c2 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d2)
+    d3 = UpSampling2D(size=(2, 2))(c2)
+    c3 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d3)
+    d4 = UpSampling2D(size=(2, 2))(c3)
+    c4 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d4)
+    d5 = UpSampling2D(size=(2, 2))(c4)
+    c5 = Conv2D(1, kernel_size=(1, 1), activation='sigmoid', padding='same')(d5)
+    output = c5
+
+    model = Model(inputs=encoder.inputs, outputs=output)
+
+    
+
+    return model
