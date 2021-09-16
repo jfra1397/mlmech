bdb866472db5e7166e90703791b30cc4e43d84d5
jfra1397 <jfra@hotmail.de>
2021-09-16 14:06:33 +0200

    more training

--- create_model.py
+++ create_model.py
@@ -4,67 +4,66 @@ from tensorflow import keras
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Concatenate, Conv2DTranspose
 
-def generate_model():
+def generate_model(img_size):
     
-    # encoder = tf.keras.applications.MobileNetV2(include_top=False, weights='imagenet', input_shape=(256, 256, 3), classifier_activation=None)
-    # encoder = tf.keras.applications.VGG16(include_top=False, weights="imagenet", input_tensor=None, input_shape=(256, 256, 3), pooling=None, classifier_activation= None)
-    encoder = tf.keras.applications.ResNet50V2(include_top=False,weights="imagenet",input_tensor=None,input_shape=None,pooling=None,classifier_activation=None)
-    encoder.trainable = False
+    # # encoder = tf.keras.applications.MobileNetV2(include_top=False, weights='imagenet', input_shape=(img_size), classifier_activation=None)
+    # encoder = tf.keras.applications.VGG16(include_top=False, weights="imagenet", input_tensor=None, input_shape=(img_size), pooling=None, classifier_activation= None)
+    # encoder.trainable = False
 
-    d1 = UpSampling2D(size=(2, 2))(encoder.layers[-1].output)
-    c1 = Conv2D(8, kernel_size=(3, 3), activation='selu', padding='same')(d1)
-    d2 = UpSampling2D(size=(2, 2))(c1)
-    c2 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d2)
-    d3 = UpSampling2D(size=(2, 2))(c2)
-    c3 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d3)
-    d4 = UpSampling2D(size=(2, 2))(c3)
-    c4 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d4)
-    d5 = UpSampling2D(size=(2, 2))(c4)
-    #c5 = Conv2D(1, kernel_size=(1, 1), activation='sigmoid', padding='same')(d5)
-    c5 = Conv2D(3, kernel_size=(1, 1), activation='softmax', padding='same')(d5)
-    output = c5
+    # d1 = UpSampling2D(size=(2, 2))(encoder.layers[-1].output)
+    # c1 = Conv2D(8, kernel_size=(3, 3), activation='selu', padding='same')(d1)
+    # d2 = UpSampling2D(size=(2, 2))(c1)
+    # c2 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d2)
+    # d3 = UpSampling2D(size=(2, 2))(c2)
+    # c3 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d3)
+    # d4 = UpSampling2D(size=(2, 2))(c3)
+    # c4 = Conv2D(16, kernel_size=(3, 3), activation='selu', padding='same')(d4)
+    # d5 = UpSampling2D(size=(2, 2))(c4)
+    # c5 = Conv2D(1, kernel_size=(1, 1), activation='sigmoid', padding='same')(d5)
+    # #c5 = Conv2D(3, kernel_size=(1, 1), activation='softmax', padding='same')(d5)
+    # output = c5
 
-    model = Model(inputs=encoder.inputs, outputs=output)
-    # complexity = 4
+    #model = Model(inputs=encoder.inputs, outputs=output)
+    complexity = 5
 
-    # x = Input((256, 256, 3))
-    # inputs = x
+    x = Input((img_size))
+    inputs = x
 
-    # #down sampling 
-    # f = 8
-    # layers = []
+    #down sampling 
+    f = 8
+    layers = []
 
-    # for i in range(0, complexity):
-    #     x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    #     x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    #     layers.append(x)
-    #     x = MaxPooling2D() (x)
-    #     f = f*2
-    # ff2 = 64 
+    for i in range(0, complexity):
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        layers.append(x)
+        x = MaxPooling2D() (x)
+        f = f*2
+    ff2 = 64 
 
-    # #bottleneck 
-    # j = len(layers) - 1
-    # x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    # x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    # x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
-    # x = Concatenate(axis=3)([x, layers[j]])
-    # j = j -1 
+    #bottleneck 
+    j = len(layers) - 1
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
+    x = Concatenate(axis=3)([x, layers[j]])
+    j = j -1 
 
-    # #upsampling 
-    # for i in range(0, complexity-1):
-    #     ff2 = ff2//2
-    #     f = f // 2 
-    #     x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    #     x = Conv2D(f, 3, activation='relu', padding='same') (x)
-    #     x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
-    #     x = Concatenate(axis=3)([x, layers[j]])
-    #     j = j -1 
+    #upsampling 
+    for i in range(0, complexity-1):
+        ff2 = ff2//2
+        f = f // 2 
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
+        x = Concatenate(axis=3)([x, layers[j]])
+        j = j -1 
 
-    # #classification 
-    # outputs = Conv2D(1, 1, activation='sigmoid') (x)
+    #classification 
+    outputs = Conv2D(1, 1, activation='sigmoid') (x)
 
-    # #model creation 
-    # model = Model(inputs=[inputs], outputs=[outputs])
+    #model creation 
+    model = Model(inputs=[inputs], outputs=[outputs])
 
     
 
